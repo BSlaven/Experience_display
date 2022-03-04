de01d7793506341faed008c01a709329ad5b34fe
Slaven B <slaven.bunijevac@gmail.com>
2022-03-04 23:43:58 +0100

    add fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
+  
+  const [ jobs, setJobs ] = useState([]);
+
+  // useEffect(() => {
+    //   console.log(jobs.company)
+    // }, [jobs]);
+    
+    const fetchJobs = async () => {
+      const res = await fetch(url);
+      const newJobs = await res.json();
+      console.log(newJobs);
+      setJobs(newJobs);
+    }
+
+    useEffect(() => {
+      fetchJobs();
+    }, []);
   
   return (
     <div className="App">
